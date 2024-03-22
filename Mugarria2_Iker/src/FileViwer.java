c56a5f83444589f4a445e304b69a2922f26ade49
Iker <irodriguezba23wg@ikzubirimanteo.com>
2024-03-22 20:41:47 +0100

    klasea sortzen

--- Mugarria2_Iker/src/FileViwer.java
+++ Mugarria2_Iker/src/FileViwer.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
-import java.awt.event.ActionListener;
 
 public class FileViwer {
-    private JComboBox<String>fitxategiComboBox;
+    private JComboBox<String> fitxategiComboBox;
     private JTextArea testuArea;
     private JButton garbituBotoia, itxiBotoia;
+
+
 
 }
