58b18ec2951919bd122588013fe46748985d9f1b
Iker <irodriguezba23wg@ikzubirimanteo.com>
2024-03-22 20:48:37 +0100

    ComboBox sortzen

--- Mugarria2_Iker/src/FileViwer.java
+++ Mugarria2_Iker/src/FileViwer.java
@@ -1,10 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FileViwer {
     private JComboBox<String> fitxategiComboBox;
     private JTextArea testuArea;
     private JButton garbituBotoia, itxiBotoia;
 
+    public FileViwer(){
+        setTitle("Proba Eventuak: Fitxategiak");
+        setSize(500,300);
+        setDefaultOperation(EXIT_ON_CLOSE);
+        setLeyout(new FlowLayout());
+
+        String[] fitxategiak = {"python.txt" , "c.txt" , "java.txt"};
+
+    }
 
 
+
+
+
+
+
+
+
+
+
+
+    private void setTitle(String s) {
+    }
 }
